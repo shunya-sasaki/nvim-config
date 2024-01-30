65683ea88225fd093007b80abc9542e13413744a
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2024-01-30 23:40:59 +0900

    Fix a bug of clipboard

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 if os_name == "wsl" then
+    vim.opt.clipboard = "unnamedplus"
     vim.g.clipboard = {
         name = 'myClipboard',
         copy = {
