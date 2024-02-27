a1de2d21cb8f64bb339c4c5f47cfcd7e1157d00d
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2024-02-27 23:37:26 +0900

    Fix a bug of the autocmd for jupyter

--- lua/plugins/extra/jupyter_plugins.lua
+++ lua/plugins/extra/jupyter_plugins.lua
@@ -2,9 +2,15 @@ return {
     {
         "jupyter-vim/jupyter-vim",
         config = function()
-            vim.api.nvim_buf_set_keymap(0, "n", "<leader>r",
-                ":JupyterSendCell<CR>",
-                { silent = true, noremap = true })
+            vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+                pattern = { "*.py" },
+                callback = function()
+                    vim.api.nvim_buf_set_keymap(0, "n", "<leader>r",
+                        ":JupyterSendCell<CR>",
+                        { silent = true, noremap = true })
+                end
+            })
+            vim.g.jupyter_highlight_cells = 0
         end
     }
 }
