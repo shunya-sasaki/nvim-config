34962cb2a50614e210c6379ffba4dfd393101df4
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2024-03-17 21:30:05 +0900

    Fix bugs in fern settings to sort pathes by their extensions

--- lua/plugins/filer_plugins.lua
+++ lua/plugins/filer_plugins.lua
@@ -1,3 +1,129 @@
+--- Rreturn extension of a file name.
+-- If the file name starts with ".", return an empty string.
+-- @param filename string
+-- @return string that indicates the extension of the file
+local function extract_extension(filename)
+    -- return an empty string if the file name starts with ".".
+    if filename:sub(1, 1) == "." then
+        return ""
+    else
+        -- extract the back from the ".".
+        return filename:match("^.+%.(.+)$") or ""
+    end
+end
+
+--- Compare two file or directory pathes by their names.
+-- If n1 has a larger name than n2, return 1
+-- that means n1 is displayed after n2.
+-- @param n1 first file or directory
+-- @param n2 second file or directory
+-- @return a int value that indicates the order of the two pathes
+local function compare_name(n1, n2)
+    if n1.name > n2.name then
+        return 1
+    elseif n1.name < n2.name then
+        return -1
+    else
+        return 0
+    end
+end
+
+--- Compare two file names by their extensions.
+-- If the file name starts with ".",
+-- it is considered to have no extension.
+-- If n1 has a larger extension than n2, return 1
+-- that means n1 is displayed after n2.
+-- @param n1 first file or directory
+-- @param n2 second file or directory
+-- @return a int value that indicates the order of the two pathes
+local function compare_extension(n1, n2)
+    local ext1 = extract_extension(n1.name)
+    local ext2 = extract_extension(n2.name)
+    if ext1 > ext2 then
+        return 1
+    elseif ext1 < ext2 then
+        return -1
+    else
+        return compare_name(n1, n2)
+    end
+end
+
+--- Compare two file or directory pathes by their status.
+-- The status is that the path is a directory or not,
+-- if the path is a file, the status is 0.
+-- And if the path is a closed directory, the status is 1.
+-- Otherwise, the path is an opened directory,
+-- the status is 2.
+-- @param n1 first file or directory
+-- @param n2 second file or directory
+-- @return a int value that indicates the order of the two pathes
+local function compare_status(n1, n2)
+    if n1.status > n2.status then
+        return -1
+    elseif n1.status < n2.status then
+        return 1
+    else
+        return compare_extension(n1, n2)
+    end
+end
+
+--- Compare two file or directory pathes by their key value.
+-- The key value is a parent directory name of the path.
+-- @param n1 first file or directory
+-- @param n2 second file or directory
+-- @param i_class the number of levels from the root directory
+-- @return a int value that indicates the order of the two pathes
+local function compare_keyvalue(n1, n2, i_class)
+    local val1 = n1.__key[i_class]
+    local val2 = n2.__key[i_class]
+    if val1 > val2 then
+        return -1
+    elseif val1 < val2 then
+        return 1
+    else
+        return 0
+    end
+end
+
+--- Compare two file or directory pathes.
+-- If the pathes have the same key value, compare their status.
+-- Otherwise, compare their key value.
+-- @param n1 first file or directory
+-- @param n2 second file or directory
+-- @param i_class the number of levels from the root directory
+-- @return a int value that indicates the order of the two pathes
+local function compare_path(n1, n2, i_class)
+    if #n1.__key == 0 or #n2.__key == 0 or
+        #n1.__key == i_class and #n2.__key == i_class then
+        return compare_status(n1, n2)
+    else
+        return compare_keyvalue(n1, n2, i_class)
+    end
+end
+
+--- Compare two file or directory pathes by their extensions.
+-- @param n1 first file or directory
+-- @param n2 second file or directory
+-- @return a int value that indicates the order of the two pathes
+local function compare(n1, n2)
+    local n_n1 = #n1.__key
+    local n_n2 = #n2.__key
+    local n_loop = math.max(n_n1, n_n2)
+    for i = 1, n_loop do
+        local result = compare_path(n1, n2, i)
+        if result ~= 0 then
+            return result
+        end
+    end
+end
+
+--- Return a comparator that compares two pathes by their extensions.
+local function extension_comparator()
+    return {
+        compare = compare
+    }
+end
+
 return {
     {
         'lambdalisue/fern.vim',
@@ -9,67 +135,10 @@ return {
                 vim.g["fern#renderer"] = "default"
             end
             vim.g["fern#default_hidden"] = 1
-            -- vim.keymap.set('n', '<C-b>', "<cmd> Fern . -reveal=% -drawer -toggle <CR>")
             vim.keymap.set('n', '<C-b>', "<cmd> Fern . -drawer -toggle <CR>")
 
-            local function extractExtension(filename)
-                -- return an empty string if the file name starts with ".".
-                if filename:sub(1, 1) == "." then
-                    return ""
-                else
-                    -- extract the back from the ".".
-                    return filename:match("^.+%.(.+)$") or ""
-                end
-            end
-
-            local function compareExtensions(a, b)
-                local extA = extractExtension(a)
-                local extB = extractExtension(b)
-                if extA > extB then
-                    return 1
-                elseif extA < extB then
-                    return -1
-                else
-                    return 0
-                end
-            end
-
-            local function compare(n1, n2)
-                local name1 = n1.name
-                local name2 = n2.name
-                local is_dir1 = n1.status -- 0: file, 1: directory
-                local is_dir2 = n2.status
-                if (is_dir1 > is_dir2) then
-                    return -1
-                elseif (is_dir1 < is_dir2) then
-                    return 1
-                else
-                    local extension_compare_result = compareExtensions(name1, name2)
-                    if extension_compare_result ~= 0 then
-                        return extension_compare_result
-                    else
-                        if (name1:sub(1, 1) == "." and name2:sub(1, 1) ~= ".") then
-                            return -1
-                        elseif (name1:sub(1, 1) ~= "." and name2:sub(1, 1) == ".") then
-                            return 1
-                        elseif name1 > name2 then
-                            return -1
-                        elseif name1 < name2 then
-                            return 1
-                        else
-                            return 0
-                        end
-                    end
-                end
-            end
-
-            local function compare_extension()
-                return {
-                    compare = compare
-                }
-            end
             vim.g['fern#comparators'] = {
-                extension = compare_extension }
+                extension = extension_comparator }
             vim.g['fern#comparator'] = "extension"
         end
     },
