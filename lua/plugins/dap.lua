83deb3f3c69d85d069e09890d1114bb1e4852c18
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2025-07-02 22:30:14 +0900

    feat(dap): add keymaps for debugging and dap-ui toggle
    - Added convenient keymaps for common nvim-dap actions (continue, step, breakpoints, REPL, widgets).
    - Integrated dap-ui with new dependencies and <Leader>d keymap for toggling the UI.
    - Improved debugging workflow and discoverability.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -75,16 +75,65 @@ return {
 				"python",
 			}
 			opts.handlers = {}
+			vim.keymap.set("n", "<F5>", function()
+				require("dap").continue()
+			end)
+			vim.keymap.set("n", "<F10>", function()
+				require("dap").step_over()
+			end)
+			vim.keymap.set("n", "<F11>", function()
+				require("dap").step_into()
+			end)
+			vim.keymap.set("n", "<F12>", function()
+				require("dap").step_out()
+			end)
+			vim.keymap.set("n", "<Leader>b", function()
+				require("dap").toggle_breakpoint()
+			end)
+			vim.keymap.set("n", "<Leader>B", function()
+				require("dap").set_breakpoint()
+			end)
+			vim.keymap.set("n", "<Leader>lp", function()
+				require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+			end)
+			vim.keymap.set("n", "<Leader>dr", function()
+				require("dap").repl.open()
+			end)
+			vim.keymap.set("n", "<Leader>dl", function()
+				require("dap").run_last()
+			end)
+			vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+				require("dap.ui.widgets").hover()
+			end)
+			vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+				require("dap.ui.widgets").preview()
+			end)
+			vim.keymap.set("n", "<Leader>df", function()
+				local widgets = require("dap.ui.widgets")
+				widgets.centered_float(widgets.frames)
+			end)
+			vim.keymap.set("n", "<Leader>ds", function()
+				local widgets = require("dap.ui.widgets")
+				widgets.centered_float(widgets.scopes)
+			end)
 		end,
 	},
 	{
 		"rcarriga/nvim-dap-ui",
-		lazy = true,
 		branch = "master",
 		dependencies = {
 			{ "mfussenegger/nvim-dap" },
+			{ "nvim-neotest/nvim-nio" },
+			{ "theHamsta/nvim-dap-virtual-text", opts = {} },
 		},
+		event = "VeryLazy",
 		opts = function()
+			vim.api.nvim_set_keymap(
+				"n",
+				"<Leader>d",
+				":lua require('dapui').toggle()<CR>",
+				{ noremap = true, silent = true }
+			)
 			local dap, dapui = require("dap"), require("dapui")
 			dap.listeners.before.attach.dapui_config = function()
 				dapui.open()
