eb15750cc1997a32721b66c00bd8b84ed464ee69
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2025-10-18 10:48:26 +0900

    chore(nvim): remove markdown-preview.nvim and bundled CSS
    - Drop iamcco/markdown-preview.nvim config from lua/plugins/preview.lua
    - Delete css/github-dark.css, github-markdown.css, monokai.css, monokai-sublime.css, tokyo-night-dark.css
    - Keep markdown-toc as the only markdown-related plugin

--- lua/plugins/preview.lua
+++ lua/plugins/preview.lua
@@ -20,35 +20,4 @@ return {
 			},
 		},
 	},
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = "cd app && npm install",
-		config = function()
-			vim.g.mkdp_theme = "dark"
-			vim.g.mkdp_markdown_css = vim.fn.stdpath("config") .. "/css/github-markdown.css"
-			vim.g.mkdp_highlight_css = vim.fn.stdpath("config") .. "/css/github-dark.css"
-			vim.g.mkdp_auto_start = 0
-			vim.g.mkdp_auto_close = 0
-			vim.g.mkdp_refresh_slow = 0
-			vim.g.mkdp_command_for_global = 0
-			vim.g.mkdp_open_to_the_world = 0
-			vim.g.mkdp_preview_options = {
-				mkit = {},
-				katex = {},
-				uml = {},
-				maid = {},
-				disable_sync_scroll = 0,
-				sync_scroll_type = "middle",
-				hide_yaml_meta = 1,
-				sequence_diagrams = {},
-				flowchart_diagrams = {},
-				content_editable = 1,
-				disable_filename = 0,
-				toc = {},
-			}
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-	},
 }
